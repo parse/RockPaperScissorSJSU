22e82ea06618d5126d16c053f2f632baaf5ce766
Anders Hassis <anders.hassis@gmail.com>
2012-03-05 19:59:29 -0800

    Proposal for UI and Game interaction

--- src/teaminternational/Game.java
+++ src/teaminternational/Game.java
@@ -12,16 +12,54 @@ public class Game {
 		AI computer 	= new AI();
 		
 		// Create throws
-		Throw t1 	= human.getThrow();
+		Throw t1 	= null;
 		Throw t2 	= computer.getThrow();
 		
-		// Compare
-		if (t1.compareTo(t2) == BEFORE) {
-			System.out.println(human.toString() + " won with " + t1.toString() + " against " + t2.toString());
-		} else if (t1.compareTo(t2) == AFTER) {
-			System.out.println(computer.toString() + " won with " + t2.toString() + " against " + t1.toString());
+		// Create UI
+		UI ui = new UI();
+		
+		Integer mainMenuChoice = ui.getMainMenu();
+		
+		if (mainMenuChoice == 1) {
+			// Start game
+			// Select number of rounds and then prompt for choice
+			Integer nrRounds = ui.getRoundsInput();
+			
+			for (int i = 1; i <= nrRounds; i++) {
+				
+				// Get human throw choice
+				Integer playerChoice = ui.getPlayerChoice();
+				
+				if (playerChoice == 1) {
+					t1 	= new Rock();
+				} else if (playerChoice == 2) {
+					t1 	= new Paper();
+				} else if (playerChoice == 3) {
+					t1 	= new Scissor();
+				} else {
+					t1 	= new Scissor();
+				}
+				
+				// Compare and calculate winner
+				if (t1.compareTo(t2) == BEFORE) {
+					System.out.println(human.toString() + " won with " + t1.toString() + " against " + t2.toString());
+				} else if (t1.compareTo(t2) == AFTER) {
+					System.out.println(computer.toString() + " won with " + t2.toString() + " against " + t1.toString());
+				} else {
+					System.out.println("Equals with " + t1.toString() );
+				}
+				
+				// @TODO: Save statistic for later
+			}
+			
+			// @TODO: Show scores and prompt for what we want to do.
+			//Integer endGameMenu = ui.getGameoverInput();
+		} else if (mainMenuChoice == 2) {
+			// Show score
+		} else if (mainMenuChoice == 3) {
+			// Show Help
 		} else {
-			System.out.println("Equals with " + t1.toString() );
+			// Fallback.
 		}
 	}
 }
--- src/teaminternational/UI.java
+++ src/teaminternational/UI.java
@@ -1,9 +1,58 @@
 package teaminternational;
-import java.io.Console;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class UI {
-	public Integer getInput() {
-		return null;
+	public Integer getMainMenu() {
+
+		System.out.println("Enter your choice, 1 for Start game, 2 for Show score, 3 for Help");
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+		String s = null;
+		
+		try {
+			s = in.readLine();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+				
+		return Integer.parseInt( s );
 		
 	}
+	
+	public Integer getPlayerChoice() {
+
+		System.out.println("Enter your choice, 1 for Rock, 2 for Paper, 3 for Scissor");
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+		String s = null;
+		
+		try {
+			s = in.readLine();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+				
+		return Integer.parseInt( s );
+		
+	}
+	
+	
+	
+	public Integer getRoundsInput() {
+
+		System.out.println("Enter number of rounds");
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+		String s = null;
+		
+		try {
+			s = in.readLine();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+				
+		return Integer.parseInt( s );
+		
+	}
+	
 }
